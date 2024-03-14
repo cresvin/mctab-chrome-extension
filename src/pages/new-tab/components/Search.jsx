a9826ab32afd02cd6b5291a51e28c4f229c8ea84
cresvin <cresvinn@gmail.com>
2024-03-14 15:19:09 +0100

    Add search functionality to the Search component

--- src/pages/new-tab/components/Search.jsx
+++ src/pages/new-tab/components/Search.jsx
@@ -1,11 +1,20 @@
 export default function Search() {
+  const handleSearch = (e) => {
+    const search = e.target.value;
+    const query = `https://www.google.com/search?q=${encodeURIComponent(
+      search
+    )}`;
+    window.location.href = query;
+  };
+
   return (
-    <div>
-      <input
-        type="text"
-        className="px-4 py-2 rounded-md outline-none focus:ring ring-yellow-500 bg-zinc-900 placeholder:text-zinc-400 w-full"
-        placeholder="Search for anything..."
-      />
-    </div>
+    <input
+      type="text"
+      className="px-4 py-2 rounded-md outline-none focus:ring ring-yellow-500 bg-zinc-900 placeholder:text-zinc-400 w-full"
+      placeholder="Search for anything..."
+      onKeyDown={(e) => {
+        if (e.key === "Enter") handleSearch(e);
+      }}
+    />
   );
 }
