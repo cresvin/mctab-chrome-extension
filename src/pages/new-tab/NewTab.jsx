0b5838f9fc8bf1bf7598a30b47dce72307624702
cresvin <cresvinn@gmail.com>
2024-03-14 15:17:22 +0100

    Add Clock component to NewTab page

--- src/pages/new-tab/NewTab.jsx
+++ src/pages/new-tab/NewTab.jsx
@@ -1,3 +1,4 @@
+import Clock from "./components/Clock";
 import NewsContainer from "./components/NewsContainer";
 import Search from "./components/Search";
 
@@ -5,6 +6,7 @@ export default function NewTab() {
   return (
     <main className="bg-grid-zinc-900 h-screen">
       <div className="container mx-auto flex flex-col p-10 h-full">
+        <Clock />
         <Search />
         <NewsContainer />
       </div>
